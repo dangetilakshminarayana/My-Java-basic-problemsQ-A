203820e5de2ec4f3e8cd48759aeb5d646a1f386a
dangetilakshminarayana <dangetilakshminarayana7@gmail.com>
2026-08-20 09:49:50 +0530

    palindromeCheck

---
+++ Strings/PalindromeCheck/PalindromeCheck.java
@@ -0,0 +1,16 @@
+package Strings.PalindromeCheck;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "racecar";
+        String reversed = new StringBuilder(str).reverse().toString();
+        
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+    
+ 
+}
